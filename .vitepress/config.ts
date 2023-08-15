e424e6d5580811099f60597c008a6024de83ffd1
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2023-08-15 17:55:21 +0200

    .vitepress/config.ts: Fix description

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Thalos",
-  description: "A VitePress Site",
+  description: "Your Antilope SHIP Provider",
   cleanUrls: true,
   outDir: "dist",
   themeConfig: {
